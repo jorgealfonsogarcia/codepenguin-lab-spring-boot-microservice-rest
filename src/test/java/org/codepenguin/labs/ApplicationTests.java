de9c134ca3783d4224fc78da1f0457646f552c6f
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-08-31 10:09:02 -0500

    :tada: (feat) Initial commit.

---
+++ src/test/java/org/codepenguin/labs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codepenguin.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
